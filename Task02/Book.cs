1d4db850beaa6a0eeeab3b6ba9c804a8693870a5
Artsiom <Artsiomxxx@gmail.com>
2016-07-13 19:00:33 +0300

    did the second task without sort method.

---
+++ Task02/Book.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Task02
+{
+    public sealed class Book: IEquatable<Book>, IComparable<Book>
+    {
+
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public int Pages { get; set; }
+        public int YearOfPublish { get; set; }
+
+        public Book(string author, string title, int pages, int yearOfPublish)
+        {
+            if(author==null || title==null)
+                throw new ArgumentNullException();
+            Author = author;
+            Title = title;
+            Pages = pages;
+            YearOfPublish = yearOfPublish;
+        }
+        public Book(Book otherBook)
+        {
+            if (ReferenceEquals(otherBook,null))
+            throw new NullReferenceException();
+            Author = otherBook.Author;
+            Title = otherBook.Title;
+            Pages = otherBook.Pages;
+            YearOfPublish = otherBook.YearOfPublish;
+        }
+
+#region implements interfaces
+        public bool Equals(Book other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return (other.Author == Author)&&(other.Title == Title)&&
+                (other.Pages == Pages)&&(other.YearOfPublish==YearOfPublish);
+        }
+
+        public int CompareTo(Book other)
+        {
+            if (other == null) return 1;
+            if (other.Pages > Pages) return 1;
+            if (other.Pages < Pages) return -1;
+            return 0;
+        }
+        #endregion
+
+#region implements object methods
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            Book book = obj as Book;
+            if (book == null)
+                return false;
+            return Equals(book);
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Book {Title} published in {YearOfPublish} by {Author} and has {Pages} pages.";
+        }
+#endregion
+    }
+}
